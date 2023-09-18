3208fa9755eaf29ee55321eca0c99cb1c5f3b83d
Anton Torkunov <anton.torkunov@cinelab.ru>
2023-09-18 15:55:54 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Systemd service for run user actions on docker container starts 
+[README_RU.md](Русская версия)
+
+## Common Info
+
+Sometimes it need to add routes to the container. It often neccessary for containers that have two or more network interfaces. Especiality if it is rootless and doesn't use custom init system like [https://github.com/just-containers/s6-overlay](s6-overlay) and so on.
+
+In this case it impossible to add routes neither during container create nor after it was started (inside container)  
+
+So the only way to add routes to the running container is to run some script on the host after container started.
+
+It usually can be done with next command:
+```docker exec -u 0 <container> <command>```
+or
+```nsenter -n -t $(docker inspect --format {{.State.Pid}} <container>) <command>```
+
+To track container start events common used ```docker events```
+
+So here is simple service that will do that.
+
+## How it works
+
+Service use ```docker events``` to catch container start events. Then it do a couple of things:
+
+1: Checks container's Labels for "docker-events.route" label. Takes it value, split to parts by semicolon and pass every part to the command:  
+```nsenter -n -t $(docker inspect --format {{.State.Pid}} <container>) ip route <value>```
+
+This method is useful because we can keep routes together with the container. Actially in the docker-compose.yaml See example bellow.
+
+2: Finds user defined ищет пользовательский скрипт с именем start.<container> и выполняет его
+
+This method is convenient because you can execute **any** commands when the container starts, not only add routes
+
+## Installation
+
+The service acts as systemd service and can be installed with:
+
+$ docker_events.sh --instal
+
+## Configuration
+
+All the necessary variables set in the begining of the script, but usually they don't need to be changed.
+
+## Example of docker-compose.yaml
+
+```
+version: "3.8"
+networks:
+  wan:
+    # In this example we create IPVLAN L2 network
+    driver: ipvlan
+    driver_opts:
+      parent: eth1
+    ipam:
+      config:
+      - subnet: 33.156.88.0/24
+        gateway: 33.156.88.1
+        ip_range: 33.156.88.128/25
+  lan: 
+    driver: bridge
+    ipam:
+      config:
+      - subnet: 172.20.20.0/24
+
+services:
+  dualnet:
+    image: someimage:latest
+
+    ports:
+      - "4000:4000"
+
+    networks: 
+      wan:
+      lan:
+    
+    labels: 
+      docker-events.route: "delete default;add default via 33.156.88.1;add 10.0.0.0/8 via 172.20.20.1;add 192.168.0.0/16 via 172.20.20.1" 
+
+```
+После запуска контейнра будут выполенены следующие команды:
+
+```
+nsenter ... ip route delete default
+nsenter ... ip route add default via 33.156.88.1
+nsenter ... ip route add 10.0.0.0/8 via 172.20.20.1
+nsenter ... ip route add 192.168.0.0/16 via 172.20.20.1
+```
+
---
+++ docker_events.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Absolute or relative path to the user defined scripts. It is usually the script directory.
+scripts_dir=.
+
+version=1.0
+app_name="Docker Events Handler"
+
+# ==================  Options bellow usually don't need to be changed ==============
+
+# systemd service file name
+servicefile=/etc/systemd/system/docker-events-handler.service
+
+# Main script will be copied to this path and name.
+maindir=/usr/lib/docker-events-handler
+mainfile=docker_events.sh
+
+# char used for split label docker-events.route to separated routes.
+splitter=";"
+
+# =======================   END OF Options  ====================== 
+
+# Check that dependics are availble
+dependics="realpath id nsenter docker"
+for dep in $dependics; do
+        command -v $dep >/dev/null 2>&1 || { echo >&2 "Error! '$dep' is not installed. Aborting."; exit 1; }
+done
+
+error=0  # Set to 1 if was error
+
+self=$(realpath $0)
+cd $(dirname $0)
+base=$(pwd)
+
+# define Usage
+usage() {
+echo "
+$app_name, v$version
+
+Usage: $0 -c client_name [--install] [--service] [--version]
+    --install   : Install itself as a service
+    --service   : Service mode. For internal use.
+    -h, --help  : show this help
+    --version   : show app version 
+"
+}
+
+# This function handle the system service
+service() {
+
+    # Scripts must be executables and named: event.name
+    
+    docker events --filter type=container --filter event=start --format '{{.Status}}.{{.Actor.Attributes.name}}' --since=15s | while read name; do
+        echo Event has been catched: $name
+
+        # check for container labels fist
+        coname=$(echo $name | cut -d. -f2)
+        
+        # getting for route labels
+        routes=$(docker inspect $coname --format '{{ index .Config.Labels "docker-events.route" }}')
+        
+        if [ ! -z "$routes" ]; then
+            coPID=$(docker inspect --format {{.State.Pid}} $coname)
+
+            IFS=$splitter
+            for routestr in $routes; do
+                echo "$coname: Found container assigned route-label and applying it: $routestr"
+                cmd="nsenter -n -t $coPID ip route $routestr"
+                sh -c "$cmd"
+            done
+        fi
+
+        # check for user defined scripts
+        if [ -f $scripts_dir/$name ]; then
+            echo "$coname: Found and running event handler: $scripts_dir/$name"
+            $scripts_dir/$name
+        else
+            echo "Event handler was not found and skipped: $scripts_dir/$name"
+        fi
+    done
+    exit 0
+}
+# END OF service()
+
+
+# Install the service and exit
+install() {
+
+echo "Installing service file ($servicefile)..."
+echo "
+[Unit]
+Description=Docker Event Handler Service
+After=docker.service
+
+[Service]
+# For old version systemd Type it must be exec
+#Type=exec
+
+ExecStart=$maindir/$mainfile --service
+
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+" >$servicefile || error=1
+
+echo "Installing main script file ($maindir/$mainfile)..."
+mkdir -p $maindir           || error=1
+cp $self $maindir/$mainfile || error=1
+
+[[ $error == 1 ]] && echo "Error(s) occured. Aborted." && exit 1
+
+systemctl daemon-reload 
+echo "Enable docker-events-handler service...."
+systemctl enable docker-events-handler
+cd $maindir
+
+echo "
+Installation complete! 
+User defined scripts path: 
+   $(realpath $scripts_dir)
+
+Start service: 
+    systemctl start docker-events-handler
+
+Show logs: 
+    journalctl -u docker-events-handler
+
+"
+}
+# END OF install()
+
+
+# ====================== Main Code section ============================
+
+# Check arguments
+[[ $# == 0 ]] && usage && exit 1
+
+doinstall=false
+doservice=false
+
+while [[ "$#" -gt 0 ]]; do case $1 in
+  --version)  echo $version; exit;;
+  --install) doinstall=true;;
+  --service) doservice=true;;
+  *) echo "Unknown parameter passed: $1"; usage; exit 1;;
+esac; shift; done
+
+
+# ======================= check for root ================================
+[[ $(id -u) -ne 0 ]] && echo "Must be run as root! Aborted." &&  exit 1
+
+echo $app_name, v$version
+echo Scripts Directory: $(realpath $scripts_dir)
+
+$doinstall && install && exit
+$doservice && service && exit
+
+#[[ $error == 1 ]] && echo "Error(s) occured. Aborted." && exit 1
